faba6744fd6af2f41cc8a422053df9d4fd5fa1f1
EuniceNam <caellime@gmail.com>
2026-05-04 15:09:01 +0900

    add: 생성수정시간 추가, 서비스 레이어 추가

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity // 아래 구조대로 DB 테이블을 만들어야 한다.
@@ -17,11 +19,15 @@ public class Post {
     @Id // PK
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private int id; // INT
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = this.createDate;
         this.title = title;
         this.content = content;
     }
