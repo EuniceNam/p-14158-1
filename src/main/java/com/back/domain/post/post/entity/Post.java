de6815f2057899a6a44d79f1b4370d80e61a500d
EuniceNam <caellime@gmail.com>
2026-05-04 14:59:27 +0900

    fix: final 제거

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -12,21 +12,17 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class Post {
     @Id // PK
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
-    private final int id; // INT
+    private int id; // INT
     private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
-        this.id = 0;
         this.title = title;
         this.content = content;
-    }
-
-    public Post() {
-        this("", "");
     }
 }
