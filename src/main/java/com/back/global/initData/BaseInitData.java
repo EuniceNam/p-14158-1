eccb42d986eb5829a41afe2ee4159b24b67546c6
EuniceNam <caellime@gmail.com>
2026-05-06 10:25:19 +0900

    add: transaction, self

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -3,28 +3,35 @@ package com.back.global.initData;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
+    @Autowired
+    @Lazy
+    private BaseInitData self;
     private final PostService postService;
     private int callCount = 0;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            work1();
-            work2();
+            self.work1();
+            self.work2();
 
             callCount++;
         };
     }
 
+    @Transactional
     void work1() {
         if (postService.count() > 0) return;
         Post post1 = new Post("제목 1", "내용 1");
@@ -34,6 +41,7 @@ public class BaseInitData {
         System.out.println("기본 데이터가 초기화되었습니다.");
     }
 
+    @Transactional(readOnly = true)
     void work2() {
         Optional<Post> opPost1 = postService.findById(1);
         // SELECT * FROM post WHERE id = 1;
