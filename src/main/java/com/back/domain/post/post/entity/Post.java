c86b8ab5f668e6139a2085ef9a189a995d588d03
EuniceNam <caellime@gmail.com>
2026-05-06 11:41:34 +0900

    add: MappedSuperclass 어노테이션 사용

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,29 +1,15 @@
 package com.back.domain.post.post.entity;
 
+import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity // 아래 구조대로 DB 테이블을 만들어야 한다.
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class) // Auditing 기능 활성화
-public class Post {
-    @Id // PK
-    @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
-    private int id; // INT
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Post extends BaseEntity {
     private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
     private String content;
